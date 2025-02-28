d9895715e55a66e202766825e1979c5b137f4630
cland29 <carl.lee.landskron@gmail.com>
2025-02-28 12:33:11 -0500

    Adjust onboard led light

--- App/TalkingSword/src/main.py
+++ App/TalkingSword/src/main.py
@@ -1,12 +1,15 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
+led = LED(47)
 
 print("Hello world")
 
-while True:
+count = 0
+
+while count < 10:
     led.on()
     sleep(1)
     led.off()
     sleep(1)
+    count += 1
