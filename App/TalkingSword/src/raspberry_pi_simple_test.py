a91c25a119fca45397b3f107755d2ed99cb1bc2d
Ry24 <Ry24@RYANS_DESKTOP>
2025-02-28 19:27:57 -0700

    34x-->345

--- App/TalkingSword/src/raspberry_pi_simple_test.py
+++ App/TalkingSword/src/raspberry_pi_simple_test.py
@@ -9,7 +9,7 @@ Adafruit BNO055 Sensor using this library
 import time
 from adafruit_extended_bus import ExtendedI2C as I2C
 import adafruit_bno055
-import adafruit_adxl34x
+import adafruit_adxl345
 
 # To enable i2c-gpio, add the line `dtoverlay=i2c-gpio` to /boot/config.txt
 # Then reboot the pi
@@ -20,19 +20,8 @@ i2c = I2C(1)
 
 sensor = adafruit_bno055.BNO055_I2C(0x28)
 
-"last_val = 0xFFFF"
-accelerometer_1 = adafruit_adxl34x.ADXL345(0x1d)
-accelerometer_2 = adafruit_adxl34x.ADXL345(0x53)
-
-def temperature():
-    global last_val  # pylint: disable=global-statement
-    result = sensor.temperature
-    if abs(result - last_val) == 128:
-        result = sensor.temperature
-        if abs(result - last_val) == 128:
-            return 0b00111111 & result
-    last_val = result
-    return result
+accelerometer_1 = adafruit_adxl345.ADXL345(0x1d)
+accelerometer_2 = adafruit_adxl345.ADXL345(0x53)
 
 
 while True:
