56973df20f644928b3c89a27af03c236da08fea9
cland29 <carl.lee.landskron@gmail.com>
2025-05-04 21:51:33 -0400

    Add accelerometer class

---
+++ App/TalkingSword/src/Hardware/accelerometer.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+import adafruit_adxl34x
+from adafruit_extended_bus import ExtendedI2C as I2C
+
+i2c = I2C(1)
+
+class Accelerometer(ABC):
+    @abstractmethod
+    def get_x(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_y(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_z(self) -> float:
+        pass
+
+class AdafruitAdxl34x(Accelerometer):
+    def __init__(self, id):
+        self.accel = adafruit_adxl34x.ADXL345(i2c, id)
+
+    @abstractmethod
+    def get_x(self) -> float:
+        return self.accel.acceleration[0]
+    
+    @abstractmethod
+    def get_y(self) -> float:
+        return self.accel.acceleration[1]
+    
+    @abstractmethod
+    def get_z(self) -> float:
+        return self.accel.acceleration[2]
+
